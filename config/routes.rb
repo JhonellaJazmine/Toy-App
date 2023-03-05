8f48d7501cba614437a8f9bd5dd1481823cc7e2d
JhonellaJazmine <orquillojhonella@gmail.com>
2023-03-05 21:16:49 +0800

    deploy app to render

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users #to create the mapping of user URLs to controller actions for the User resource.
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'application#hello'
+  # root 'application#hello'
+  root 'users#index'
 end
